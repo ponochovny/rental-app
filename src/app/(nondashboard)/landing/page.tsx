2725bf1663013f7c4f2be38961ae1322213a4929
ponochovny <ponochovny@gmail.com>
2025-03-27 17:16:36 +0200

    feat: Discover Section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,3 +1,4 @@
+import DiscoverSection from './DiscoverSection'
 import FeaturesSection from './FeaturesSection'
 import HeroSection from './HeroSection'
 
@@ -6,6 +7,7 @@ const page = () => {
 		<div>
 			<HeroSection />
 			<FeaturesSection />
+			<DiscoverSection />
 		</div>
 	)
 }
