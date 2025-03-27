e78e9415ae8bb50aa0247d93661fc686b2f93ac0
ponochovny <ponochovny@gmail.com>
2025-03-27 18:32:37 +0200

    feat: footer section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,6 +1,7 @@
 import CallToActionSection from './CallToActionSection'
 import DiscoverSection from './DiscoverSection'
 import FeaturesSection from './FeaturesSection'
+import FooterSection from './FooterSection'
 import HeroSection from './HeroSection'
 
 const page = () => {
@@ -10,6 +11,7 @@ const page = () => {
 			<FeaturesSection />
 			<DiscoverSection />
 			<CallToActionSection />
+			<FooterSection />
 		</div>
 	)
 }
