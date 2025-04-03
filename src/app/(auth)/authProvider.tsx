5f570e514af99afd696b7c4d6b9812eddf3c2dd8
ponochovny <ponochovny@gmail.com>
2025-04-03 22:55:41 +0300

    feat: amplify, auth provider

---
+++ src/app/(auth)/authProvider.tsx
@@ -0,0 +1,148 @@
+import React from 'react'
+import { Amplify } from 'aws-amplify'
+
+import {
+	Authenticator,
+	Heading,
+	Radio,
+	RadioGroupField,
+	useAuthenticator,
+	View,
+} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure({
+	Auth: {
+		Cognito: {
+			userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID!,
+			userPoolClientId:
+				process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_CLIENT_ID!,
+		},
+	},
+})
+
+const components = {
+	Header() {
+		return (
+			<View className='mt-4 mb-7'>
+				<Heading level={3} className='!text-2xl !font-bold'>
+					RENT
+					<span className='text-secondary-500 font-light hiver:!text-primary-300'>
+						IFUL
+					</span>
+				</Heading>
+				<p className='text-muted-foreground mt-2'>
+					<span className='font-bold'>Welcome!</span> Please sign in to continue
+				</p>
+			</View>
+		)
+	},
+	SignIn: {
+		Footer() {
+			const { toSignUp } = useAuthenticator()
+			return (
+				<View className='text-center mt-4'>
+					<p className='text-muted-foreground'>
+						Don&apos;t have an account?{' '}
+						<button
+							onClick={toSignUp}
+							className='text-primary hover:underline bg-transparent border-none !font-bold'
+						>
+							Sign up here
+						</button>
+					</p>
+				</View>
+			)
+		},
+	},
+	SignUp: {
+		FormFields() {
+			const { validationErrors } = useAuthenticator()
+			return (
+				<>
+					<Authenticator.SignUp.FormFields />
+					<RadioGroupField
+						legend='Role'
+						name='custom:role'
+						errorMessage={validationErrors?.['custom:role']}
+						hasError={!!validationErrors?.['custom:role']}
+						isRequired
+					>
+						<Radio value='tenant'>Tenant</Radio>
+						<Radio value='manager'>Manager</Radio>
+					</RadioGroupField>
+				</>
+			)
+		},
+		Footer() {
+			const { toSignIn } = useAuthenticator()
+			return (
+				<View className='text-center mt-4'>
+					<p className='text-muted-foreground'>
+						Already have an account?{' '}
+						<button
+							onClick={toSignIn}
+							className='text-primary hover:underline bg-transparent border-none !font-bold'
+						>
+							Sign in
+						</button>
+					</p>
+				</View>
+			)
+		},
+	},
+}
+
+const formFields = {
+	signIn: {
+		username: {
+			placeholdre: 'Enter your email',
+			label: 'Email',
+			isRequired: true,
+		},
+		password: {
+			placeholder: 'Enter your password',
+			label: 'Password',
+			isRequired: true,
+		},
+	},
+	signUp: {
+		username: {
+			order: 1,
+			placeholdre: 'Choose your username',
+			label: 'Username',
+			isRequired: true,
+		},
+		email: {
+			order: 2,
+			placeholder: 'Enter your email address',
+			label: 'Email',
+			isRequired: true,
+		},
+		password: {
+			order: 3,
+			placeholder: 'Create your password',
+			label: 'Password',
+			isRequired: true,
+		},
+		confirm_password: {
+			order: 4,
+			placeholder: 'Confir your password',
+			label: 'Confirm password',
+			isRequired: true,
+		},
+	},
+}
+
+function Auth({ children }: { children: React.ReactNode }) {
+	// const { user } = useAuthenticator((context) => [context.user])
+	return (
+		<div className='h-full'>
+			<Authenticator components={components} formFields={formFields}>
+				{() => <>{children}</>}
+			</Authenticator>
+		</div>
+	)
+}
+
+export default Auth
